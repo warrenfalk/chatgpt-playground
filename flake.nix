c5c232b2a767b04f1a93092a44764f9602e62938
Warren Falk <warren@warrenfalk.com>
2023-07-09 09:01:05 -0400

    nix node environment

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    #flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, flake-utils, nixpkgs }:
+    # instead of duplicating the same configuration for every system
+    # eachDefaultSystem will iterate over the default list of systems
+    # and then map the outputs of devShells = { default } to devShells = { <system> = default }
+    # 
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = import nixpkgs {
+          system = system;
+          config = {
+            allowUnfree = true;
+          };
+        };
+        in {
+          devShells = {
+            default = pkgs.mkShellNoCC {
+              buildInputs = [
+                # put packages for development shells here
+                pkgs.nodejs-18_x
+                pkgs.yarn
+                pkgs.jq
+              ];
+            };
+          };
+        }
+      );
+}
